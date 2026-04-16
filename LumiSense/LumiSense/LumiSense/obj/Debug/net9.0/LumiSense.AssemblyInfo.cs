5be88db9b8e1876455d569fe7cae513d50c47f1f
Kjiop <hasan.metin.2006@gmail.com>
2026-04-16 21:14:27 +0300

    added a calculator for kwh

--- LumiSense/LumiSense/LumiSense/obj/Debug/net9.0/LumiSense.AssemblyInfo.cs
+++ LumiSense/LumiSense/LumiSense/obj/Debug/net9.0/LumiSense.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LumiSense")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6e58672ec540a780fe8980de58fe0563a929f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7103d9f9479d49804bb240937a272c7657bd7eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LumiSense")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LumiSense")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
